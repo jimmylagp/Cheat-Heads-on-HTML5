1a6f99d8f92424432cd4b1e3c7eccb2797355d77
Luis Antonio Gomez Prieto <lgomez@digizent.com>
2014-06-18 23:00:53 -0500

    Add more heads, bounce animation and draggable limit.

--- js/draggable.js
+++ js/draggable.js
@@ -1,5 +1,6 @@
 (function($) {
   
+  /*Draggable function*/
   $('.chat-heads').draggable({
     //axis: "y",
     scroll: false,
@@ -11,24 +12,28 @@
       setTimeout(function(){
         ui.helper.unbind("click.prevent");}, 300);
 
-        var window_width = $(window).width();
-        var head_postion = $(this).position().left;
-        var orientation = {};
-
-        if( head_postion > (window_width / 2) )
-        {
-          orientation = {left: (window_width-50)+"px"};
-        }
-        else
-        {
-          orientation = {left: "-5px"};
-        }
-
-        $(this).animate(orientation, 300 );
+        positioner($(this));
 
       }
   });
 
+  /*Reposition*/
+  $(window).resize(function() {
+
+    var window_width = $(window).width();
+    var chats = $('.chat-heads');
+    chats.each(function(i) {
+
+      if( $(chats[i]).position().left > (window_width)/2 )
+      {
+        $(chats[i]).css({ left: "auto", right: "-5px" });
+      }
+
+    });
+
+  });
+
+  /*Open chat*/
   $('.chat-heads').click(function (e) {
     var chatMessage = $(this).find('.message');
     //First, hide any open messages that aren't us
@@ -41,10 +46,48 @@
   
   //Make sure our chat heads don't stack
   $(document).ready(function() {
+    
     var chats = $('.chat-heads');
-    chats.each(function(i) {
-      $(chats[i]).css('top', i*60);
+    var i = 1;
+    chats.each(function() {
+
+      $(this).css({ top: i*5 });
+      i++;
     });
+
   });
+
+
+  /*Positioner*/
+  function positioner(heads)
+  {
+    var window_width = $(window).width();
+    var window_height = $(window).height();
+    var head_wpostion = heads.position().left;
+    var head_hposition = heads.position().top;
+
+    if( head_wpostion > (window_width / 2) )
+    {
+      heads.animate({left: (window_width-40)+"px"}, 300 );
+      heads.animate({left: (window_width-50)+"px"}, 300 );
+    }
+    else
+    {
+      heads.animate({left: "-15px"}, 300 );
+      heads.animate({left: "-5px"}, 300 );
+    }
+
+    if( head_hposition > (window_height - 50) )
+    {
+      heads.animate({top: (window_height-75)+"px"}, 200 );
+      heads.animate({top: (window_height-65)+"px"}, 200 );
+    }
+
+    if( head_hposition < 0 )
+    {
+      heads.animate({top: "15px"}, 200 );
+      heads.animate({top: "5px"}, 200 );
+    }
+  }
 
 })(jQuery);
