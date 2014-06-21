c8f425e8ea1adc0c3547092efe1314e71c990191
Luis Antonio Gomez Prieto <lgomez@digizent.com>
2014-06-20 20:04:49 -0500

    Add new jQuery Plugin for dragging elements, new animations and toggle click functionality.

---
+++ js/drag.js
@@ -0,0 +1,123 @@
+// Draggable plugin
+(function($) {
+    $.fn.drag = function(options) {
+        options = $.extend({
+            handle: null,
+            cursor: 'move',
+            draggingClass: 'dragging',
+            heads: null
+        }, options);
+
+        var status = true;
+        var $handle = this,
+            $drag = this;
+
+        if( options.handle ) {
+            $handle = $(options.handle);
+        }
+
+        $handle.css('cursor', options.cursor).on("mousedown", function(e) {
+            var x = $drag.offset().left - e.pageX,
+                y = $drag.offset().top - e.pageY,
+                z = $drag.css('z-index'),
+                oldx = $drag.offset().left,
+                oldy = $drag.offset().top;
+
+            $drag.css('z-index', 100000);
+
+            $(document.documentElement).on('mousemove.drag', function(e) {
+                var head_wpostion = $(options.heads).position().left;
+                var head_hposition = $(options.heads).position().top;
+                var chats = $(options.heads);
+
+                chats.each(function(i) {
+                        $(chats[i]).css({ left: $(chats[i]).next().position().left});
+                        $(chats[i]).css({top: $(chats[i]).next().position().top});
+                });
+
+                $drag.offset({
+                    left: x + e.pageX,
+                    top: y + e.pageY
+                });
+            })
+            .one('mouseup', function() {
+                $(this).off('mousemove.drag');
+                $drag.css('z-index', z);
+
+                if($drag.offset().left == oldx && $drag.offset().top == oldy)
+                {
+                    var chats = $(options.heads);
+                    console.log(status);
+                    if(status)
+                    {
+                        status = false;
+                        chats.each(function(i) {
+                            $(chats[i]).animate({ left: 70*(i+1), top: 30 }, 100);
+                        });
+                    }
+                    else
+                    {
+                        status = true;
+                        positioner();
+                    }
+                }
+                else
+                {
+                    positioner();
+                }
+            });
+
+            // disable selection
+            e.preventDefault();
+        });
+
+        /*Reposition*/
+        $(window).resize(function() {
+            var window_width = $(window).width();
+            var chats = $(options.heads);
+            chats.each(function(i) {
+
+                if( $(chats[i]).position().left > (window_width)/2 )
+                {
+                    $(chats[i]).css({ left: "auto", right: "-5px" });
+                }
+
+            });
+
+        });
+
+
+        function positioner(head_wpostion, head_hposition)
+        {
+            var window_width = $(window).width();
+            var window_height = $(window).height();
+            var head_wpostion = $(options.heads).position().left;
+            var head_hposition = $(options.heads).position().top;
+
+            if( head_wpostion > (window_width / 2) )
+            {
+              $(options.heads).animate({left: (window_width-40)+"px"}, 300 );
+              $(options.heads).animate({left: (window_width-50)+"px"}, 300 );
+            }
+            else
+            {
+              $(options.heads).animate({left: "-15px"}, 300 );
+              $(options.heads).animate({left: "-5px"}, 300 );
+            }
+
+            if( head_hposition > (window_height - 50) )
+            {
+              $(options.heads).animate({top: (window_height-75)+"px"}, 200 );
+              $(options.heads).animate({top: (window_height-65)+"px"}, 200 );
+            }
+
+            if( head_hposition < 0 )
+            {
+              $(options.heads).animate({top: "15px"}, 150 );
+              $(options.heads).animate({top: "5px"}, 150 );
+            }
+        }
+    };
+ 
+
+})(jQuery);
